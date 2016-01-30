4bec58707ec72d0922622808ea2c181169a08762
Guilherme Bayer <Guilherme Bayer>
2016-01-30 15:20:35 -0200

    added changes to link colors in menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function($) { 
-    $(".scroll").click(function(event){        
-        event.preventDefault();
-        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
-   });
+  $(".scroll").click(function(event){        
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+ });
 });
 
 $(document).ready(function() {
@@ -16,10 +16,13 @@ $(document).ready(function() {
   $(window).scroll(function() {
   	var menuLink = $('#menu a');
     var scroll   = $(this).scrollTop();
+    console.log(scroll);
 
     if(scroll > 585 && scroll < 1230) {
     	menuLink.css({'color': '#000'});
-    } else if(scroll > 1440) {
+    } else if(scroll > 1440 && scroll < 2095) {
+      menuLink.css({'color': '#000'});
+    } else if(scroll > 2295) {
       menuLink.css({'color': '#000'});
     } else {
     	menuLink.css({'color': '#fff'});
