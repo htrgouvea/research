bc024c617d212ac9faa86874ed851854c2b28a70
HeitorG <contato.htr@hotmail.com>
2016-01-30 13:53:51 -0200

    subindo site

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+jQuery(document).ready(function($) { 
+    $(".scroll").click(function(event){        
+        event.preventDefault();
+        $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+   });
+});
+
+$(document).ready(function() {
+	/* style for scroll bar */ 
+	$('html').niceScroll({
+		cursorborder: '#000',
+		cursorcolor:  '#000'
+	});
+
+	/* for changes in menu link colors */ 
+  $(window).scroll(function() {
+  	var menuLink = $('#menu a');
+    var scroll   = $(this).scrollTop();
+
+    if(scroll > 585 && scroll < 1230) {
+    	menuLink.css({'color': '#000'});
+    } else if(scroll > 1440) {
+      menuLink.css({'color': '#000'});
+    } else {
+    	menuLink.css({'color': '#fff'});
+    };
+  });
+});
+
+
