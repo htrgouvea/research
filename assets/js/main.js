28f1b4780828064a2f98def1e311592ff7039d9a
HeitorG <hi@heitorgouvea.me>
2016-03-10 12:19:18 -0300

    :smile: :metal: added favi-icon

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,24 @@ $(document).ready(function() {
 		cursorborder: '#00aeef',
 		cursorcolor:  '#00aeef'
 	});
+
+	$(window).scroll(function() {
+  		var menuLink = $('# a');
+    	var scroll   = $(this).scrollTop();
+    	
+    	console.log(scroll);
+
+    	if(scroll == 585) {
+    		menuLink.css({'color': '#000'});
+    	}
+    	else if(scroll > 1440 && scroll < 2095) {
+      		menuLink.css({'color': '#000'});
+    	}
+    	else if(scroll > 2295) {
+      		menuLink.css({'color': '#000'});
+    	}
+    	else {
+    		menuLink.css({'color': '#fff'});
+    	};
+  	});
 });
