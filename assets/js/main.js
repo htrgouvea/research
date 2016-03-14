5298e3cba4ea5a44514a714ffcfa4b5176b2518e
HeitorG <hi@heitorgouvea.me>
2016-03-14 19:11:28 -0300

    :smile: :metal: update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,5 +2,6 @@ var app = angular.module('myApp', ['duScroll']);
 
 app.controller('myAppCtrl', function($scope, $document) {
 	$scope.toTop = function() {}
+	
     $scope.toDown = function() {}
 });
