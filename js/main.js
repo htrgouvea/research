3d92347d3c3e58e7e0486bab6cc4a6e317ef94fc
GouveaHeitor <hi@heitorgouvea.me>
2018-04-17 10:51:40 -0300

    upload adjusts

--- js/main.js
+++ js/main.js
@@ -1,17 +1,9 @@
 $(document).ready(function(){
-
-
-	$("#portfolio-contant-active").mixItUp();
-
-
 	$("#testimonial-slider").owlCarousel({
-	    paginationSpeed : 500,      
+	    paginationSpeed : 500,
 	    singleItem:true,
 	    autoPlay: 3000,
 	});
-
-
-
 
 	$("#clients-logo").owlCarousel({
 		autoPlay: 3000,
@@ -19,14 +11,6 @@ $(document).ready(function(){
 		itemsDesktop : [1199,5],
 		itemsDesktopSmall : [979,5],
 	});
-
-	$("#works-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
-
 
 	// google map
 		var map;
@@ -37,17 +21,9 @@ $(document).ready(function(){
 		  });
 		}
 
-
 	// Counter
-
 	$('.counter').counterUp({
         delay: 10,
         time: 1000
     });
-
-
 });
-
-
-
-
