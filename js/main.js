02d786c8bb3ebfc6142dbe16e14ef0e469908f36
GouveaHeitor <hi@heitorgouvea.me>
2020-04-29 13:55:39 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,3 +3,7 @@ var host = "heitorgouvea.me"
 if (window.location.host == host && window.location.protocol != "https:") {
 	window.location.protocol = "https:"
 }
+
+if (window.location.host != host) {
+       alert('1337');
+}
